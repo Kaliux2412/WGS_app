e58d5401e39952e9f369412758b3b6a8edb6b98a
Kaliux2412 <kgmoralesa24@gmail.com>
2024-04-14 13:12:01 -0600

    Actualización2

---
+++ lib/componentes/modelos/plantas.dart
@@ -0,0 +1,15 @@
+// ignore: file_names
+class Plantas {
+  final String name;
+  final String cuidado;
+  final String imagen;
+  final String descripcion;
+
+  Plantas({
+    required this.name,
+    required this.cuidado,
+    required this.imagen,
+    required this.descripcion,
+  });
+// ignore: empty_constructor_bodies
+}
