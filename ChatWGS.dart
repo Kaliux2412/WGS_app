5562b2ff27836228182eece5494e505b8ab6dbef
Morisa24 <92126255+Kaliux2412@users.noreply.github.com>
2024-04-15 22:14:11 -0600

    ChatFuncional

--- ChatWGS.dart
+++ ChatWGS.dart
@@ -1,32 +1,32 @@
-// import 'package:dash_chat_2/dash_chat_2.dart';
-// import 'package:flutter/material.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
 
-// class ChatWGS extends StatefulWidget {
-//   const ChatWGS({super.key});
+class ChatWGS extends StatefulWidget {
+  const ChatWGS({super.key});
 
-//   @override
-//   State<ChatWGS> createState() => _ChatWGSState();
-// }
+  @override
+  State<ChatWGS> createState() => _ChatWGSState();
+}
 
-// class _ChatWGSState extends State<ChatWGS> {
+class _ChatWGSState extends State<ChatWGS> {
 
-//   final ChatUser _currentUser = 
-//     ChatUser(id: '1', firstName: 'WGS User');
+  final ChatUser _currentUser = 
+    ChatUser(id: '1', firstName: 'WGS User');
 
-//   final ChatUser _ChatUser = 
-//     ChatUser(id: '2', firstName: 'WGS Assistant');
+  final ChatUser _ChatUser = 
+    ChatUser(id: '2', firstName: 'WGS Assistant');
 
-//   List<ChatMessage> _messages = <ChatMessage>[];
+  List<ChatMessage> _messages = <ChatMessage>[];
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body:  DashChat(currentUser: _currentUser, onSend: (ChatMessage m){
-//         getChatReponse(m);
-//       }, messages: _messages),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  DashChat(currentUser: _currentUser, onSend: (ChatMessage m){
+        getChatReponse(m);
+      }, messages: _messages),
       
-//     );
-//   }
-//   Future<void> getChatReponse(ChatMessage m) async {
-//   }
-// }
+    );
+  }
+  Future<void> getChatReponse(ChatMessage m) async {
+  }
+}
